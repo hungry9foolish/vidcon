b3152ed4ca4fa88373f02f22469d5bb71d6b22ba
hungry9foolish <hungry9foolish@gmail.com>
2019-12-12 22:09:16 -0500

    removing unused dependencies

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    entry: './server.js',
-    output: {
-      path: __dirname + './lib',
-      filename: 'server.js',
-      library: 'vidcon',
-      libraryTarget: 'umd',
-    }
-  };
